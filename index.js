841a67d761f91d2af23a8cb4433396e6350fbabc
kaziabulhasib <abulhasibkazi@gmail.com>
2024-06-01 11:25:39 +0530

    connect with mongodb

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+//------------------------------------------------------------------------------------------------------
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggulbwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+//------------------------------------------------------------------------------------------------------------------
 app.get("/", (req, res) => {
   res.send("TalkRoute server is running......");
 });
