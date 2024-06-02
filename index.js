9cfa8475c472972071957853345acf80e2ddb1ce
kaziabulhasib <abulhasibkazi@gmail.com>
2024-06-02 16:55:13 +0530

    get single post

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 //------------------------------------------------------------------------------------------------------
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggulbwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,12 +24,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     //------------------------------------------------------------
-    const postsCollection = client.db("TalkRouteDb").collection("Posts");
+    const postsCollection = client.db("TalkRouteDb").collection("posts");
     // get all posts
     app.get("/posts", async (req, res) => {
       const result = await postsCollection.find().toArray();
       res.send(result);
     });
+
+    // get on post with id
+
+    app.get("posts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await postsCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
     //------------------------------------------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
